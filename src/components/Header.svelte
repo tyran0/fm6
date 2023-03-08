472cce9990a277974e16487e7a2f21d68d1ec575
tyran0 <119806764+tyran0@users.noreply.github.com>
2023-03-08 19:08:36 +0400

    Header Comp. Clean Up

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -8,7 +8,7 @@
 
 <header>
     <nav aria-label="Primary menu">
-        <div>
+        <span>
             <button aria-expanded="false">
                 <span>Open the menu</span>
                 <svg width="16" height="15" viewBox="0 0 16 15">
@@ -21,29 +21,29 @@
                 <a href="/">About</a>
                 <a href="/">Contact</a>
             </ul>
-            <a href="/">
-                <svg width="138" height="20" viewBox="0 0 138 20">
-                    <use href="#svg_logo"></use>
-                </svg>
-            </a>
-            <button>
-                <span>Open your cart</span>
-                <svg width="22" height="20" viewBox="0 0 22 20">
-                    <use href="#svg_icon-cart"></use>
-                </svg>
-            </button>
-            <button aria-hidden="true">
-                <!--
-                    Might be an accessability concern.
-                    Hard to tell If visually-impaired people might need to use
-                    profile picture functionality, and because of that Im not entirely sure
-                    whether to include a button with exactly the same title as the previous one,
-                    hence why I've decided to put `aria-hidden="true"`.
-                -->
-                <span>Open your cart</span>
-                <img src="./images/image-avatar.png" alt=""
-                     width="44" height="44">
-            </button>
-        </div>
+        </span>
+        <a href="/">
+            <svg width="138" height="20" viewBox="0 0 138 20">
+                <use href="#svg_logo"></use>
+            </svg>
+        </a>
+        <button>
+            <span>Open your cart</span>
+            <svg width="22" height="20" viewBox="0 0 22 20">
+                <use href="#svg_icon-cart"></use>
+            </svg>
+        </button>
+        <button aria-hidden="true">
+            <!--
+                Might be an accessability concern.
+                Hard to tell If visually-impaired people might need to use
+                profile picture functionality, and because of that Im not entirely sure
+                whether to include a button with exactly the same title as the previous one,
+                hence why I've decided to put `aria-hidden="true"`.
+            -->
+            <span>Open your cart</span>
+            <img src="./images/image-avatar.png" alt=""
+                    width="44" height="44">
+        </button>
     </nav>
 </header>
